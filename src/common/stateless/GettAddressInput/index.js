82c3bcf5f2eaca0d049aae00f17f05a1f392af3f
Dor Shay <dor@poob.me>
2017-05-04 12:07:02 +0300

    Stage 1 - Adress autocomplete'

--- src/common/stateless/GettAddressInput/index.js
+++ src/common/stateless/GettAddressInput/index.js
@@ -3,7 +3,10 @@ import { TextInput, View } from 'react-native';
 import { Navigation } from 'react-native-navigation';
 
 import GoogleMapsAutoComplete from '../GoogleMapsAutoComplete';
-import GettTextInput from '../GettTextInput'
+import GettTextAutocomplete from '../GettTextAutocomplete'
+
+import Qs from 'qs';
+
 
 Navigation.registerComponent('GettAddressInput.selectMode', () => GoogleMapsAutoComplete);
 
@@ -11,40 +14,45 @@ Navigation.registerComponent('GettAddressInput.selectMode', () => GoogleMapsAuto
 export default class GettAddressInput extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {options: []};
   }
 
   onAddressSelect(value) {
-    this.props.navigator.pop()
-    this.setState({AddressData: value, selectMode: false})
+    this.setState({AddressData: value, options: []})
   }
 
   getValue() {
     return this.state.AddressData ? this.state.AddressData.description : null;
   }
 
-  onChangeText(e) {
-    console.log(e)
+  onChangeText(text) {
+    qs = Qs.stringify({
+      key: 'AIzaSyDRCKG5dE1gay5wa7L-Xyols-orobOSAxQ',
+      input: text,
+    })
 
-    // this.props.navigator.push({
-    //   screen: 'GettAddressInput.selectMode',
-    //   title: this.props.label,
-    //   passProps: {
-    //     onSelect: this.onAddressSelect.bind(this),
-    //     value: this.getValue(),
-    //     placeholder: this.props.placeholder,
-    //     onSelect: this.onAddressSelect.bind(this)
-    //   }
-    // });
+    fetch(`https://maps.googleapis.com/maps/api/place/autocomplete/json?${qs}`, { method: 'GET', })
+    .then((response) => response.json())
+    .then((responseJson) => {
+      const options = responseJson.predictions.map(option => {
+        option.text = option.description
+        return option;
+      })
+
+      this.setState({ options })
+    })
   }
 
   render() {
+    console.log(this.state.options)
     return (
-      <GettTextInput
+      <GettTextAutocomplete
         onChangeText={this.onChangeText.bind(this)}
+        onSelect={this.onAddressSelect.bind(this)}
         placeholder={this.props.placeholder}
         value={ this.getValue() }
         label={this.props.label}
+        options={this.state.options}
       />
     );
   }
--- src/common/stateless/GettTextAutocomplete/index.js
+++ src/common/stateless/GettTextAutocomplete/index.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { TextInput, View } from 'react-native';
+import { TextInput, View, Text, StyleSheet, TouchableHighlight } from 'react-native';
 import { Navigation } from 'react-native-navigation';
 
 import GoogleMapsAutoComplete from '../GoogleMapsAutoComplete';
@@ -14,26 +14,27 @@ export default class GettAddressInput extends Component {
     this.state = {};
   }
 
-  onAddressSelect(value) {
-    this.props.navigator.pop()
-    this.setState({AddressData: value, selectMode: false})
-  }
-
   getValue() {
     return this.state.AddressData ? this.state.AddressData.description : null;
   }
 
-  onInputPress() {
-    this.props.navigator.push({
-      screen: 'GettAddressInput.selectMode',
-      title: this.props.label,
-      passProps: {
-        onSelect: this.onAddressSelect.bind(this),
-        value: this.getValue(),
-        placeholder: this.props.placeholder,
-        onSelect: this.onAddressSelect.bind(this)
+  onSelect(value) {
+    return () => {
+      this.setState({focused:false})
+      if(this.props.onSelect) {
+        this.props.onSelect(value)
       }
-    });
+    }
+  }
+
+  onFocus() {
+    this.setState({focused:true})
+  }
+
+  onChangeText(text) {
+    if(this.props.onChangeText) {
+      this.props.onChangeText(text)
+    }
   }
 
   render() {
@@ -41,24 +42,45 @@ export default class GettAddressInput extends Component {
     return (
       <View>
         <GettTextInput
-          onFocus={ this.onInputPress.bind(this) }
-          placeholder={this.props.placeholder}
-          onChangeText={ this.onChangeText }
-          value={ this.getValue() }
+          onFocus={ this.onFocus.bind(this) }
+          placeholder={ this.props.value ||  this.props.placeholder }
+          onChangeText={ this.onChangeText.bind(this) }
+          value={ this.state.focused ?  null : this.props.value }
           label={this.props.label}
         />
-      (
+      {
         options.length > 0 ?
-          <View>
-            options.map( (option) => {
-              return <Text> option.text </Text>
-            })
-          </View>
+          (
+            <View style={{padding: 10, paddingRight: 30, paddingLeft: 30}}>
+              {
+                options.map((option) =>  {
+                  return (
+                    <TouchableHighlight onPress={this.onSelect(option).bind(this)} key={option.text}>
+                      <View style={styles.box}>
+                        <Text style={styles.text}> {option.text} </Text>
+                      </View>
+                    </TouchableHighlight>
+                  )
+                })
+            }
+            </View>
+          )
           :
           null
-      )
-
+      }
       </View>
     );
   }
 }
+
+var styles = StyleSheet.create({
+  box: {
+    height: 50,
+    padding: 10,
+    borderRadius: 3,
+    borderTopColor: '#000000',
+    borderTopWidth: 1,
+  },
+  text: {
+  }
+});
