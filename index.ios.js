cd03c313fd09870a0a01fb7ccf188c7fda308f99
Oren Schwartz <OrenSchwartz@gmail.com>
2017-05-04 18:33:39 +0300

    some more

--- index.ios.js
+++ index.ios.js
@@ -8,31 +8,38 @@ import {
 import { Navigation } from 'react-native-navigation';
 
 import { registerScreens } from './src/screens';
-registerScreens(); // this is where you register all of your app's screens
+import FlowManager from 'lib/flow_manager'
 
-// start the app
-Navigation.startTabBasedApp({
-  tabs: [
-    {
-      label: 'Registration',
-      screen: 'app.Registration', // this is a registered name for a screen
-      // icon: require('../img/one.png'),
-      // selectedIcon: require('../img/one_selected.png'), // iOS only
-      title: 'Gett On'
-    },
-    {
-      label: 'RideDetails',
-      screen: 'app.RideDetails',
-      // icon: require('../img/two.png'),
-      // selectedIcon: require('../img/two_selected.png'), // iOS only
-      title: 'RideDetails'
-    },
-    {
-      label: 'New Rides',
-      screen: 'app.NewRide',
-      // icon: require('../img/two.png'),
-      // selectedIcon: require('../img/two_selected.png'), // iOS only
-      title: 'New Rides'
-    }
-  ]
-});
+registerScreens(); // this is where you register all of your app's screens
+const flowManager = new FlowManager()
+const phuneNum = flowManager.getPhoneNumber()
+
+function goToPage(hasActiveRides){
+  // start the app
+  Navigation.startTabBasedApp({
+    tabs: [
+      {
+        label: 'Registration',
+        screen: 'app.Registration', // this is a registered name for a screen
+        // icon: require('../img/one.png'),
+        // selectedIcon: require('../img/one_selected.png'), // iOS only
+        title: 'Gett On'
+      },
+      {
+        label: 'RideDetails',
+        screen: 'app.RideDetails',
+        // icon: require('../img/two.png'),
+        // selectedIcon: require('../img/two_selected.png'), // iOS only
+        title: 'RideDetails'
+      },
+      {
+        label: 'New Rides',
+        screen: 'app.NewRide',
+        // icon: require('../img/two.png'),
+        // selectedIcon: require('../img/two_selected.png'), // iOS only
+        title: 'New Rides'
+      }
+    ]
+  });
+}
+flowManager.getRiderRides({phuneNum, () => goToPage(true), () => goToPage(false) []})
--- src/lib/flow_manager.js
+++ src/lib/flow_manager.js
@@ -5,27 +5,27 @@ export default class FlowManager {
     this.SharedRideRequestAPI = new SharedRideRequestAPI()
   }
 
-  saveInStore({key, val, successMsg, failureMsg}){
+  async saveInStore({key, val, successMsg, failureMsg}){
     console.log({key:val});
     try {
-      AsyncStorage.setItem(key, val);
+      await AsyncStorage.setItem(key, val);
       console.log(successMsg)
     } catch (error) {
       console.log(FailureMsg)
     }
   }
 
-  load(){
+  async getPhoneNumber(){
     try {
-      const phone_num = AsyncStorage.getItem('splitter:phone_num');
+      const phone_num = await AsyncStorage.getItem('splitter:phone_num');
       if (phone_num !== null){
         // We have data!!
-        console.log("FlowManager.load: phone number retrieved from local storage")
+        console.log("FlowManager.getPhoneNumber: phone number retrieved from local storage")
         console.log(phone_num);
       }
     } catch (error) {
       // Error retrieving data
-      console.log("FlowManager.load: phone number retrieval error")
+      console.log("FlowManager.getPhoneNumber: phone number retrieval error")
       console.log(phone_num);
     }
 
@@ -41,5 +41,15 @@ export default class FlowManager {
 
   createSharedRide( ride ){
     this.SharedRideRequestAPI.create({sharedRideRequest:ride})
+  }
+
+  getRiderRides({phuneNum, successCallback, failureCallback}){
+      this.SharedRideRequestAPI.get({id:phuneNum, successCallback, failureCallback})
+  }
+
+  getRiderRidesForever({phuneNum, successCallback, failureCallback}){
+    setInterval(function() {
+      this.SharedRideRequestAPI.get({id:phuneNum, successCallback, failureCallback})
+    }, 1000);
   }
 }
